15bbbf2186f70f2ba17e98b0ab47d348efbf247a
Adam Abed <adamprog13@gmail.com>
2025-07-26 15:04:31 +0800

    Update book.js

--- models/book.js
+++ models/book.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    author: { type: String, required: true },
-    category: { type: String, required: true },
-    image: { type: String }, // Optional book cover
-    pdf: { type: String, required: true}
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  category: { type: String, required: true },
+  image: { type: String }, // Optional book cover
+  pdf: { type: String, required: true },
+  
+  // 🆕 New fields
+  description: { type: String },
+  pages: { type: Number },
+  language: { type: String },
+  year: { type: String }
+  
 }, { timestamps: true });
 
 const Book = mongoose.models.Book || mongoose.model('Book', bookSchema);
