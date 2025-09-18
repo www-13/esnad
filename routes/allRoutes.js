162fb2e26df0c9b46328143d6114945a3f49786e
Adam Abed <adamprog13@gmail.com>
2025-09-18 13:45:38 +0800

    Update allRoutes.js

--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({ default: f }) => f(...args));
 const router = express.Router();
 const User = require('../models/customerSchema');
 const Book = require('../models/book');  // Adjust the path if needed
